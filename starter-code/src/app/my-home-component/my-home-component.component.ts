bcde694fcfcb06f7df38976d10ff76c796ba444d
ahmouda12 <ahmouda12@ufl.edu>
2018-08-13 00:31:35 -0400

    iteration2

---
+++ starter-code/src/app/my-home-component/my-home-component.component.ts
@@ -0,0 +1,21 @@
+import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { MoviesService } from '../movies.service'
+
+@Component({
+  selector: 'app-my-home-component',
+  templateUrl: './my-home-component.component.html',
+  styleUrls: ['./my-home-component.component.css']
+})
+export class MyHomeComponentComponent implements OnInit {
+
+  movies: Array<Object> = [];
+
+  constructor(private router: Router, private moviesService: MoviesService) { }
+
+  ngOnInit() {
+    this.movies = this.moviesService.getMovies()
+  }
+
+
+}
